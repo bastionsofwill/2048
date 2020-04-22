31ff663898bcedaf74391c4c25c85fac1f0bbbb6
JY <bastionsofwill@gmail.com>
2020-04-22 14:01:04 +0900

    test.js is now main.js

--- main.js
+++ main.js
@@ -1,272 +1,203 @@
-let field = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-let snapShot = [];
-let numOfMoves = 0;
-let score = 0;
+var i, j, dir, randomIndex, initZeroFlag, initZero;
+var newSquare = [];
+var equals = [];
+var tempRow = [];
+var tempBoard = [];
+var newBoard = [];
+var touchStart, touchEnd;
+var deltaX, deltaY;
 
-function initiateGame() {
-    field = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-    numOfMoves = 0;
-    score = 0;
-    newNumber(true);
-    updateGrid();
+const Keycode = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
 }
 
-function convertKeyCodeToInput(keyCode) {
-    if(keyCode === 38) {
-        return 0;
-    } else if(keyCode === 40) {
-        return 1;
-    } else if(keyCode === 37) {
-        return 2;
-    } else if(keyCode === 39) {
-        return 3;
-    } else {
-        console.log("invalid keyCode: " + keyCode);
-        return -1;
-    }
+const State = {
+    PLAYING: 0,
+    GAMEOVER: 1,
+    WIN: 2,
+    INFINITE: 3
 }
 
-function newNumber(flag) {
-    if(flag) {
-        let emptySquares = [];
-        let isFour = (Math.random() > 0.9);
-
-        for(let r = 0; r < 4; r++)
-            for(let c = 0; c < 4; c++)
-                if(!field[r][c]) emptySquares.push([r, c]);
-        
-        let newSquare = emptySquares[Math.floor(emptySquares.length*Math.random())];
-        field[newSquare[0]][newSquare[1]] = isFour ? 4 : 2;
-    }
-}
-
-function compress(input) {
-    let initZero;
-    let initZeroFlag;
-    switch(input) {
-        case 0:
-            for(let j = 0; j < 4; j++) {
-                initZeroFlag = false;
-                initZero = -1;
-                for(let i = 0; i < 4; i++) {
-                    if(!field[i][j] && !initZeroFlag) {
-                        initZeroFlag = true;
-                        initZero = i;
-                    } else if(field[i][j] && initZeroFlag) {
-                        initZeroFlag = false;
-                        field[initZero][j] = field[i][j];
-                        field[i][j] = 0;
-                        i = initZero;
-                    }
+let game = {
+    gameBoard: [],
+    emptySquares: [],
+    numOfMoves: 0,
+    score: 0,
+    gameState: null,
+    createNewNumber() {
+        this.emptySquares = [];
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 4; j++) {
+                if(!this.gameBoard[i][j]) this.emptySquares.push({x: i, y: j});
+            }
+        }
+        randomIndex = Math.floor(Math.random()*this.emptySquares.length);
+        newSquare = this.emptySquares[randomIndex];
+        this.gameBoard[newSquare.x][newSquare.y] = (Math.random() > 0.9)? 4 : 2;
+        this.emptySquares.splice(randomIndex, 1);
+        console.log("emptysquares: " + this.emptySquares.length);
+    },
+    showCurrentGame() {
+        document.getElementById("moves").innerHTML = "Moves: " + this.numOfMoves;
+        document.getElementById("score").innerHTML = "Score: " + this.score;
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 4; j++) {
+                document.getElementById(i+"x"+j).innerHTML = this.gameBoard[i][j] ? this.gameBoard[i][j] : "";
+            }
+        }
+    },
+    initiateGame() {
+        this.gameBoard = [[0, 0, 0, 0], 
+                          [0, 0, 0, 0], 
+                          [0, 0, 0, 0], 
+                          [0, 0, 0, 0]];
+        this.numOfMoves = 0;
+        this.score = 0;
+        this.gameState = State.PLAYING;
+        this.createNewNumber();
+        this.showCurrentGame();
+        document.getElementById("grid").style.opacity = "1.0";
+        document.getElementById("win").style.visibility = "hidden";
+        document.getElementById("gameover").style.visibility = "hidden";
+    },
+    transpose(board) {
+        return board[0].map((elementOf0thRow, index) => board.map(row => row[index]));
+    },
+    symmetricTransform(board) {
+        return board.map((row, index) => row.map((element, index, arr) => arr[arr.length - 1 - index]));
+    },
+    convertBoard(keycode, board) {
+        tempBoard = JSON.parse(JSON.stringify(board));
+        if(!(keycode%2)) tempBoard = this.transpose(board);
+        if(keycode > 38) tempBoard = this.symmetricTransform(tempBoard);
+        return tempBoard;
+    },
+    recoverBoard(keycode, board) {
+        tempBoard = JSON.parse(JSON.stringify(board));
+        if(keycode > 38) tempBoard = this.symmetricTransform(tempBoard);
+        if(!(keycode%2)) tempBoard = this.transpose(tempBoard);
+        return tempBoard;
+    },
+    pushLeft(board) {
+        // push
+        for(i = 0; i < 4; i++) {
+            initZeroFlag = false;
+            initZero = -1;
+            for(j = 0; j < 4; j++) {
+                if(!board[i][j] && !initZeroFlag) {
+                    initZeroFlag = true;
+                    initZero = j;
+                } else if(board[i][j] && initZeroFlag) {
+                    initZeroFlag = false;
+                    board[i][initZero] = board[i][j];
+                    board[i][j] = 0;
+                    j = initZero;
                 }
             }
-            break;
-        case 1:
-            for(let j = 0; j < 4; j++) {
-                initZeroFlag = false;
-                initZero = -1;
-                for(let i = 3; i >= 0; i--) {
-                    if(!field[i][j] && !initZeroFlag) {
-                        initZeroFlag = true;
-                        initZero = i;
-                    } else if(field[i][j] && initZeroFlag) {
-                        initZeroFlag = false;
-                        field[initZero][j] = field[i][j];
-                        field[i][j] = 0;
-                        i = initZero;
-                    }
+        }
+        return board;
+    },
+    mergeLeft(board) {
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 3; j++) {
+                if(board[i][j] === board[i][j+1]) {
+                    this.score += 2*board[i][j];
+                    board[i][j] *= 2;
+                    board[i][j+1] = 0;
                 }
             }
-            break;
-        case 2:
-            for(let i = 0; i < 4; i++) {
-                initZeroFlag = false;
-                initZero = -1;
-                for(let j = 0; j < 4; j++) {
-                    if(!field[i][j] && !initZeroFlag) {
-                        initZeroFlag = true;
-                        initZero = j;
-                    } else if(field[i][j] && initZeroFlag) {
-                        initZeroFlag = false;
-                        field[i][initZero] = field[i][j];
-                        field[i][j] = 0;
-                        j = initZero;
-                    }
-                }
-            }
-            break;
-        case 3:
-            for(let i = 0; i < 4; i++) {
-                initZeroFlag = false;
-                initZero = -1;
-                for(let j = 3; j >= 0; j--) {
-                    if(!field[i][j] && !initZeroFlag) {
-                        initZeroFlag = true;
-                        initZero = j;
-                    } else if(field[i][j] && initZeroFlag) {
-                        initZeroFlag = false;
-                        field[i][initZero] = field[i][j];
-                        field[i][j] = 0;
-                        j = initZero;
-                    }
-                }
-            }
-            break;
-    }
-    
-}
-
-function moveAndMerge(input) {
-    snapShot = JSON.parse(JSON.stringify(field));
-    compress(input);
-    // Up or Down : Column-wise operation
-    if(input === 0 || input === 1) {
-        for(let c = 0; c < 4; c++) {
-            if(field[3*input][c] === field[input + 1][c]) {
-                field[3*input][c] += field[input + 1][c];
-                score += field[3*input][c];
-                let new1 = 0;
-                if(field[-1*input + 2][c] === field[-3*input + 3][c]) {
-                    new1 = field[-1*input + 2][c] + field[-3*input + 3][c];
-                    score += new1;
-                } else new1 = field[-1*input + 2][c];
-                let new2 = field[-1*input + 2][c] === field[-3*input + 3][c] ? 0 : field[-3*input + 3][c];
-                field[input + 1][c] = new1;
-                field[-1*input + 2][c] = new2;
-                field[-3*input + 3][c] = 0;
-                continue;
-            }
-            if(field[input + 1][c] === field[-1*input + 2][c]) {
-                field[input + 1][c] += field[-1*input + 2][c];
-                score += field[input + 1][c];
-                field[-1*input + 2][c] = field[-3*input + 3][c];
-                field[-3*input + 3][c] = 0;
-                continue;
-            }
-            let new2 = 0;
-            if(field[-1*input + 2][c] === field[-3*input + 3][c]) {
-                new2 = field[-1*input + 2][c] + field[-3*input + 3][c];
-                score += new2;
-            } else new2 = field[-1*input + 2][c];
-            let new3 = field[-1*input + 2][c] === field[-3*input + 3][c] ? 0 : field[-3*input + 3][c];
-            field[-1*input + 2][c] = new2;
-            field[-3*input + 3][c] = new3;
         }
-    }
-
-    // Left or Right: Row-wise operation
-    else if(input === 2 || input === 3) {
-        for(let r = 0; r < 4; r++) {
-            compress(input, r);
-            if(field[r][3*input - 6] === field[r][input - 1]) {
-                field[r][3*input - 6] += field[r][input - 1];
-                score += field[r][3*input - 6];
-                if(field[r][-1*input + 4] === field[r][-3*input + 9]) {
-                    new1 = field[r][-1*input + 4] + field[r][-3*input + 9];
-                    score += new1;
-                } else new1 = field[r][-1*input + 4];
-                let new2 = field[r][-1*input + 4] === field[r][-3*input + 9] ? 0 : field[r][-3*input + 9];
-                field[r][input - 1] = new1;
-                field[r][-1*input + 4] = new2;
-                field[r][-3*input + 9] = 0;
-                continue;
-            }
-            if(field[r][input - 1] === field[r][-1*input + 4]) {
-                field[r][input - 1] += field[r][-1*input + 4];
-                score += field[r][input - 1];
-                field[r][-1*input + 4] = field[r][-3*input + 9];
-                field[r][-3*input + 9] = 0;
-                continue;
-            }
-            let new2 = 0;
-            if(field[r][-1*input + 4] === field[r][-3*input + 9]) {
-                new2 = field[r][-1*input + 4] + field[r][-3*input + 9];
-                score += new2;
-            } else new2 = field[r][-1*input + 4];
-            let new3 = field[r][-1*input + 4] === field[r][-3*input + 9] ? 0 : field[r][-3*input + 9];
-            field[r][-1*input + 4] = new2;
-            field[r][-3*input + 9] = new3;
-            compress(input, r);
-        }
-    }
-
-    if(JSON.stringify(snapShot) !== JSON.stringify(field)) {
-        numOfMoves++;
-        return true;
-    } else {
-        console.log(JSON.stringify(snapShot));
-        console.log(JSON.stringify(field));
-    }
-    return false;
-}
-
-function updateGrid() {
-    for(let i = 0; i < 4; i++) {
-        for(let j = 0; j < 4; j++) {
-            document.getElementById(i+"x"+j).innerHTML = field[i][j] ? field[i][j] : "";
-        }
-    } 
-}
-
-function isUpdatable() {
-    for(let r = 0; r < 4; r++) {
-        for(let c = 0; c < 3; c++) {
-            if(field[r][c] === field[r][c+1]) {
-                console.log("updatable");
-                return true;
+        return board;
+    },
+    moveLeft(board) {
+        return this.pushLeft(this.mergeLeft(this.pushLeft(board)));
+    },
+    checkSameSquares() {
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 3; j++) {
+                if(this.gameBoard[i][j] === this.gameBoard[i][j+1] || this.gameBoard[j][i] === this.gameBoard[j+1][i]) return true;
             }
         }
-    }
-    for(let r = 0; r < 3; r++) {
-        for(let c = 0; c < 4; c++) {
-            if(field[r][c] === field[r+1][c]) {
-                console.log("updatable");
-                return true;
-            }
-        }
-    }
-    console.log("not updatable!");
-    return false;
-}
-
-function checkHealth() {
-    let emptyCount = 0;
-    for(let r = 0; r < 4; r++) {
-        for(let c = 0; c < 4; c++) {
-            if(field[r][c] === 2048) {
-                document.getElementById("grid").style.opacity = "0.2";
-                document.getElementById("win").style.visibility = "visible";
-                console.log("YOU WIN!!");
-                return false;
-            }
-            if(!field[r][c]) emptyCount++;
-        }
-    }
-    if(emptyCount === 0 && !isUpdatable()) {
-        document.getElementById("grid").style.opacity = "0.2";
-        document.getElementById("gameover").style.visibility = "visible";
-        console.log("YOU LOSE...")
         return false;
+    },
+    examineState() {
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 4; j++) {
+                if(this.gameState === State.PLAYING && this.gameBoard[i][j] === 2048) {
+                    this.gameState = State.WIN;
+                    document.getElementById("grid").style.opacity = "0.2";
+                    document.getElementById("win").style.visibility = "visible";
+                    return;
+                }
+            }
+        }
+        if(!this.emptySquares.length && !this.checkSameSquares()) {
+            this.gameState = State.GAMEOVER;
+            document.getElementById("grid").style.opacity = "0.2";
+            document.getElementById("gameover").style.visibility = "visible";
+        }
+    },
+    setInfiniteMode() {
+        document.getElementById("grid").style.opacity = "1.0";
+        document.getElementById("win").style.visibility = "hidden";
+        this.gameState = State.INFINITE;
+        return;
     }
-    return true;
 }
 
-function isChanged(input) {
-
-}
-
-// Initiation
-initiateGame();
-
-// Controller
-window.onkeyup = () => {
-    let keyboardInput = this.convertKeyCodeToInput(this.event.keyCode);
-    if(this.checkHealth() && keyboardInput !== -1) {
-        this.newNumber(this.moveAndMerge(keyboardInput));
-        document.getElementById("moves").innerHTML = "Moves: " + numOfMoves;
-        document.getElementById("score").innerHTML = "Score: " + score;
-        updateGrid();
-        this.checkHealth();
+function playGame(direction) {
+    if(game.gameState === State.PLAYING || game.gameState === State.INFINITE) {
+        newBoard = game.recoverBoard(direction, game.moveLeft(game.convertBoard(direction, game.gameBoard)));
+        if(JSON.stringify(game.gameBoard) !== JSON.stringify(newBoard)) {
+            game.numOfMoves++;
+            game.gameBoard = newBoard;
+            game.createNewNumber();
+            game.showCurrentGame();
+            game.examineState();
+        }
     }
-    return;
 }
+function resetGame() {
+    game.initiateGame();
+}
+function continueGame() {
+    game.setInfiniteMode();
+}
+
+game.initiateGame();
+window.onkeyup = evt => {
+    evt.preventDefault();
+    dir = evt.keyCode;
+    playGame(dir);
+}
+window.onkeydown = () => {
+    event.preventDefault();
+}
+
+document.getElementById('contentWrapper').addEventListener('touchstart', evt => {
+    evt.preventDefault();
+    touchStart = evt.targetTouches[0];
+})
+
+document.getElementById('contentWrapper').addEventListener('touchmove', evt => {
+    evt.preventDefault();
+    touchEnd = evt.targetTouches[0];
+})
+
+document.getElementById('contentWrapper').addEventListener('touchend', evt => {
+    evt.preventDefault();
+    dir = undefined;
+    deltaX = touchEnd.clientX - touchStart.clientX;
+    deltaY = touchEnd.clientY - touchStart.clientY;
+    if(Math.abs(deltaX) > 2* Math.abs(deltaY)) {
+        if(deltaX > 0) dir = Keycode.RIGHT;
+        else dir = Keycode.LEFT;
+    } else if(Math.abs(deltaY) > 2* Math.abs(deltaX)) {
+        if(deltaY > 0) dir = Keycode.DOWN;
+        else dir = Keycode.UP;
+    }
+    if(dir) playGame(dir);
+})
